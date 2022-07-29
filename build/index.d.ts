662c9d8108488be0ed0658d86e37bec1658d0d10
Egor Timoshenko <poter.t1mox@gmail.com>
2022-07-29 11:06:45 +0300

    add plugin version 1.0.0

---
+++ build/index.d.ts
@@ -0,0 +1,7 @@
+import { ConfigPlugin } from "@expo/config-plugins";
+export declare type Props = {
+    locationPermission?: string;
+    motionPermission?: string;
+};
+declare const _default: ConfigPlugin<Props>;
+export default _default;
---
+++ build/withHyperTrackAndroid.d.ts
@@ -0,0 +1,7 @@
+import { ConfigPlugin } from "@expo/config-plugins";
+import { Props } from ".";
+export declare const withAndroidPackage: ConfigPlugin;
+export declare function addMaven(src: string): import("@expo/config-plugins/build/utils/generateCode").MergeResults;
+export declare const withAndroidPackagingOptions: ConfigPlugin;
+export declare function addAndroidPackagingOptions(src: string): import("@expo/config-plugins/build/utils/generateCode").MergeResults;
+export declare const withHyperTrackAndroid: ConfigPlugin<Props>;
